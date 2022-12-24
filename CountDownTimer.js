c7d5d1b25988a58d71f6712809397204f0d5782f
solaris0051 <docodoco.0713@outlook.com>
2022-12-25 06:39:23 +0900

    chore

--- CountDownTimer.js
+++ CountDownTimer.js
@@ -6,10 +6,7 @@ export const CountDownTimer = (cdt) => {
     workerID0.postMessage(cdt);
     workerID0.addEventListener("message", (event) => {
       document.getElementById("mins_secs").textContent = event.data;
-<<<<<<< HEAD
 			event.data === "00:00" ? workerID0.terminate() : true;
-=======
->>>>>>> 0b2e455d6b84c3311a9557e945fec4c42d744caf
     });
   } else {
     document.getElementById(
