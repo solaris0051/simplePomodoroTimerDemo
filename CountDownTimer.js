0b2e455d6b84c3311a9557e945fec4c42d744caf
solaris0051 <docodoco.0713@outlook.com>
2022-12-25 05:44:32 +0900

    added CDT.js again.

---
+++ CountDownTimer.js
@@ -0,0 +1,15 @@
+export const CountDownTimer = (cdt) => {
+  if (window.Worker) {
+    const workerID0 = new Worker("./TimeCalc.js", {
+      type: "module",
+    });
+    workerID0.postMessage(cdt);
+    workerID0.addEventListener("message", (event) => {
+      document.getElementById("mins_secs").textContent = event.data;
+    });
+  } else {
+    document.getElementById(
+      "mins_secs"
+    ).textContent = `Web workerが利用可能か、ご確認ください。`;
+  }
+};
