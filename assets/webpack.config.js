776d3167e2e1927564d9d3c259649f765b996d07
solaris0051 <30798030+solaris0051@users.noreply.github.com>
2023-05-08 17:28:06 +0900

    devdep and config updated

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -70,10 +70,6 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpe?g|gif)$/i,
-        type: "asset/resource",
-      },
-      {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
         generator: {
