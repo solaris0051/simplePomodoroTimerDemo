1870b01a2cf483b9392376addb537bdd84e71997
solaris0051 <30798030+solaris0051@users.noreply.github.com>
2025-03-09 18:43:08 +0900

    refac.

--- src/TimeCalc.js
+++ src/TimeCalc.js
@@ -1,13 +1,17 @@
 addEventListener("message", event => {
   let due = event.data * 60;
-  let timerID0 = setInterval(() => {
+  const timerID0 = setInterval(() => {
     if (due >= 0) {
-      const mins = `${0}${Math.floor((due % (60 * 60)) / 60)}`.slice(-2);
-      const secs = `${0}${Math.floor(due % 60)}`.slice(-2);
-      postMessage(mins + `:` + secs);
-      due = --due;
+      const mins = formatTime(Math.floor((due % (60 * 60)) / 60));
+      const secs = formatTime(Math.floor(due % 60));
+      postMessage(`${mins}:${secs}`);
+      due--;
     } else {
       clearInterval(timerID0);
     }
   }, 1000);
 });
+
+function formatTime(time) {
+  return `0${time}`.slice(-2);
+}
